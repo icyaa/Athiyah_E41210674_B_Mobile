dbe5d5adcb90c4cb502c845b7e7908a5cc4bf0be
icyaa <e41210674@student.polije.ac.id>
2023-05-04 08:58:18 +0700

    JSON API | MovieDB

---
+++ Minggu9/app/src/main/java/com/example/moviedb/ApiEndpoint.java
@@ -0,0 +1,13 @@
+package com.example.moviedb;
+
+public class ApiEndpoint {
+
+    public static String BASEURL = "https://api.themoviedb.org/3/";
+    public static String APIKEY =  "api_key=bf7c1c07cf29fb2f97ecea85a1f2b784";
+    public static String LANGUAGE = "&language=en-US";
+    public static String SEARCH_MOVIE = "search/movie?";
+    public static String QUERY = "&query=";
+    public static String MOVIE_PLAYNOW = "movie/now_playing?";
+    public static String MOVIE_POPULAR = "discover/movie?";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w780/";
+}
